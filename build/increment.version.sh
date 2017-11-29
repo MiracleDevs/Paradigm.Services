48a1c6c30540ecda977987326a55d3567232625b
pablo@miracledevs.com <pablo@miracledevs.com>
2017-11-29 10:44:52 -0300

    Updated project versions.

--- build/increment.version.sh
+++ build/increment.version.sh
@@ -10,8 +10,8 @@ newVersion=$2
 if [[ "$oldVersion" == "" ]]; then error "Missing old version number." ; buildFailed ; fi
 if [[ "$newVersion" == "" ]]; then error "Missing new version number." ; buildFailed ; fi
 
-findVersion="<Version>$oldVersion</Version>"
-replaceVersion="<Version>$newVersion</Version>"
+findVersion="<Version>$oldVersion<\/Version>"
+replaceVersion="<Version>$newVersion<\/Version>"
 
 block "$index - Incrementing solution version from $oldVersion to $newVersion"
-execute "sed -i -e s/$findVersion/$replaceVersion/g src/**/*.csproj"
+execute "sed -i -e s/$findVersion/$replaceVersion/g ../src/**/*.csproj"
